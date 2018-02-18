f3556a2386722e43de4e5b478513df7e22672199
AIS <AIS@Simcorp.com>
2018-02-18 15:48:38 +0200

    Signed-off-by: AIS <AIS@Simcorp.com>

--- src/tetris/Tetris.java
+++ src/tetris/Tetris.java
@@ -8,12 +8,19 @@ package tetris;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableRow;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.CornerRadii;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 /**
@@ -25,6 +32,9 @@ public class Tetris extends Application {
     @Override
     public void start(Stage primaryStage) {
         Pane root = new Pane();
+        Pane leftPane = new Pane();
+        StackPane rightPane = new StackPane();
+        
         Button[][] btnField = new Button[20][10];
         double w = 20, h = 20; 
         for (int i=0;i<20;i++)
@@ -32,11 +42,30 @@ public class Tetris extends Application {
                 Button b = new Button();
                 b.setPrefSize(w, h);
                 btnField[i][j] = b;
-                root.getChildren().add(b);
+                leftPane.getChildren().add(b);
                 b.relocate(j*w+j*0.5,i*h+i*4.5);
             }
         
-        Scene scene = new Scene(root, 400, 520);
+        
+        ComboBox<Block> listOfBlock = new ComboBox<Block>();        
+        
+        
+        
+        h = 520;
+        
+        int leftWidth = (int)(w*10+w*0.5);
+        leftPane.setBackground(new Background(new BackgroundFill(Color.web("#DDF"), CornerRadii.EMPTY, Insets.EMPTY)));
+        leftPane.setPrefSize(leftWidth,h);
+        leftPane.relocate(0,0);
+        
+        int rightWidth = 200; 
+        rightPane.setBackground(new Background(new BackgroundFill(Color.web("#EFE"), CornerRadii.EMPTY, Insets.EMPTY)));
+        rightPane.setPrefSize(rightWidth, h);
+        rightPane.relocate(leftWidth,0);
+        root.getChildren().add(leftPane);
+        root.getChildren().add(rightPane);
+        
+        Scene scene = new Scene(root, 400, h);
         
         btnField[1][1].setText("*");
         
@@ -44,41 +73,7 @@ public class Tetris extends Application {
         primaryStage.setScene(scene);
         primaryStage.show();
         
-        /*Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
-            
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
-            }
-        });*/
-        /*
-        TableView table = new TableView();
-        for (int i=0; i<10;i++){
-            TableColumn<Object,String> tc = new <Object,String>TableColumn("");
-            table.getColumns().add(tc);
-        }
         
-        for (int i=0; i<20;i++){
-            TableRow tr = new TableRow();
-            table.getItems().add(tr);
-        }
-        
-        for (int i=0;i<20;i++)
-            for (int j=0; j<10; j++){
-                Item i = new Item();
-                table.
-            }
-        StackPane root = new StackPane();
-        root.getChildren().add(table);
-        
-        Scene scene = new Scene(root, 300, 250);
-        
-        primaryStage.setTitle("Hello World!");
-        primaryStage.setScene(scene);
-        primaryStage.show();
-        */
     }
 
     /**
